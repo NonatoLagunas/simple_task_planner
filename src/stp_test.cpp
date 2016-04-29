348a3f98c68d2f64a18e6cee1244d3c2a07f86cc
Nonato <rnonato.lagunas@gmail.com>
2016-04-29 17:59:47 -0500

    lang understanding tasks testing

--- src/stp_test.cpp
+++ src/stp_test.cpp
@@ -1,3 +1,4 @@
+#include "simple_task_planner/langunderstandingtasks.h"
 #include "simple_task_planner/speechgeneratortasks.h"
 #include "ros/ros.h"
 #include <iostream>
@@ -8,8 +9,13 @@ int main(int argc, char** argv)
     ros::init(argc, argv, "simple_task_planner");
     ros::NodeHandle nodeHandler;
     SpeechGeneratorTasks spgExecuter;
+    LangUnderstandingTasks langUndExecuter;
 
-    spgExecuter.syncSpeech("sync test", 5000);
+    std::string parseResult;
+    std::string input("robot follow me");
+    langUndExecuter.parseSentence(input, parseResult);
+    std::cout << "P result: " << parseResult << std::endl;
+    //spgExecuter.syncSpeech("sync test", 5000);
 
-    spgExecuter.asyncSpeech("async test");
+    //spgExecuter.asyncSpeech("async test");
 }
