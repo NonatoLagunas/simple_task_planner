c3eacb16585cada19e6b55112a07ec9a24eb2d8a
Nonato <rnonato.lagunas@gmail.com>
2016-05-04 02:33:25 -0500

    testing the headstatus class

--- src/stp_test.cpp
+++ src/stp_test.cpp
@@ -1,5 +1,6 @@
 #include "simple_task_planner/langunderstandingtasks.h"
 #include "simple_task_planner/speechgeneratortasks.h"
+#include "simple_task_planner/headstatus.h"
 #include "ros/ros.h"
 #include <iostream>
 #include <string>
@@ -8,6 +9,10 @@ int main(int argc, char** argv)
 {
     ros::init(argc, argv, "simple_task_planner");
     ros::NodeHandle nodeHandler;
+    std::cout << "sub 1 " << std::endl;
+    HeadStatus hdStatus(nodeHandler);
+    std::cout << "sub 2 " << std::endl;
+    HeadStatus hdStatus2(nodeHandler);
     SpeechGeneratorTasks spgExecuter;
     LangUnderstandingTasks langUndExecuter;
 
@@ -16,6 +21,7 @@ int main(int argc, char** argv)
     langUndExecuter.parseSentence(input, parseResult);
     std::cout << "P result: " << parseResult << std::endl;
     //spgExecuter.syncSpeech("sync test", 5000);
+    ros::spin();
 
     //spgExecuter.asyncSpeech("async test");
 }
