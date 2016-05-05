0ba4dfa763155c557f7af1332b9eb1a34ef9d207
Nonato <rnonato.lagunas@gmail.com>
2016-05-05 10:01:12 -0500

    implemented subscriber and callback to sprec topic

--- include/simple_task_planner/speechrecognitionstatus.cpp
+++ include/simple_task_planner/speechrecognitionstatus.cpp
@@ -1,7 +1,15 @@
 #include "simple_task_planner/speechrecognitionstatus.h"
 
 SpeechRecognitionStatus::SpeechRecognitionStatus(ros::NodeHandle &nh,
-        std::string sprecRecoSentencesTopic) : 
-    m_sprecRecoSentencesTopic(sprecRecoSentencesTopic)
+        std::string recoSentencesTopic) : 
+    m_recoSentencesTopic(recoSentencesTopic)
 {
+    ros::Subscriber subHeadCurrentPose = nh.subscribe(m_recoSentencesTopic,
+            100, &SpeechRecognitionStatus::recoSentenceCallback, this);
+}
+
+void SpeechRecognitionStatus::recoSentenceCallback(const 
+        hri_msgs::RecognizedSpeech::ConstPtr &recoSentenceMsg)
+{
+
 }
