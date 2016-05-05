cc6dbf92dc2204d61b1087b20a8250efc2c0f273
Nonato <rnonato.lagunas@gmail.com>
2016-05-05 12:05:06 -0500

    speech recognition listen mode implemented (not tested yet)

--- include/simple_task_planner/speechrecognitionstatus.cpp
+++ include/simple_task_planner/speechrecognitionstatus.cpp
@@ -4,12 +4,48 @@ SpeechRecognitionStatus::SpeechRecognitionStatus(ros::NodeHandle &nh,
         std::string recoSentencesTopic) : 
     m_recoSentencesTopic(recoSentencesTopic)
 {
+    m_listenActivated = false;
+
     ros::Subscriber subHeadCurrentPose = nh.subscribe(m_recoSentencesTopic,
             100, &SpeechRecognitionStatus::recoSentenceCallback, this);
 }
 
 void SpeechRecognitionStatus::recoSentenceCallback(const 
-        hri_msgs::RecognizedSpeech::ConstPtr &recoSentenceMsg)
+        hri_msgs::RecognizedSpeech::ConstPtr &recoSentencesMsg)
 {
+    clearRecoSentencesQueue(m_lastRecoSentencesQueue);
 
+    int hypothesisCount = recoSentencesMsg->hypotesis.size();
+    for(int i=0; i<hypothesisCount; i++)
+    {
+        RecognizedSentence currentHypothesis(recoSentencesMsg->hypotesis[i], 
+                recoSentencesMsg->confidence[i]);
+        m_lastRecoSentencesQueue.push(currentHypothesis);
+
+        if(m_listenActivated) 
+            m_listenRecoSentencesQueue.push(currentHypothesis);
+    }
+}
+
+void SpeechRecognitionStatus::startListening()
+{
+    m_listenActivated = true;
+}
+
+void SpeechRecognitionStatus::stopListening()
+{
+    m_listenActivated = false;
+}
+
+void SpeechRecognitionStatus::clearRecoSentencesQueue (
+        std::queue<RecognizedSentence> &queue)
+{
+    std::queue<RecognizedSentence> emptyQueue;
+    std::swap( queue, emptyQueue );
+}
+
+void SpeechRecognitionStatus::clearListenedSentencesQueue()
+{
+    if(m_listenActivated) 
+        clearRecoSentencesQueue(m_listenRecoSentencesQueue);
 }
--- include/simple_task_planner/speechrecognitionstatus.h
+++ include/simple_task_planner/speechrecognitionstatus.h
@@ -11,11 +11,45 @@
 #ifndef _JUSTINA_SPRECSTATUS_H
 #define _JUSTINA_SPRECSTATUS_H
 #include <string>
+#include <queue>
 #include "ros/ros.h"
 #include "hri_msgs/RecognizedSpeech.h"
 class SpeechRecognitionStatus
 {
     private:
+
+        /**
+         * @struct RecognizedSentence 
+         * @brief A structure to represent a recognized sentence tuple.
+         * 
+         * @var RecognizedSentence::hypothesis Contains a hypothetical 
+         * recognized sentence.
+         * @var RecognizedSentence::confidence Contains the confidence of the 
+         * recognized sentence.
+         */
+        struct RecognizedSentence
+        {
+            std::string hypothesis;
+            double confidence;
+            /**
+             * @brief Creates a new RecognizedSpeech tuple.
+             *
+             * @param newHypothesis The hypothesis to store.
+             * @param newConfidence The hypothesis asociated confidence.
+             */
+            RecognizedSentence(std::string newHypothesis, double newConfidence):
+                hypothesis(newHypothesis), confidence(newConfidence) {}
+        };
+
+        std::queue<RecognizedSentence> 
+            m_lastRecoSentencesQueue; /**< Stores the last recognized
+                                        hypothesis */
+        std::queue<RecognizedSentence> 
+            m_listenRecoSentencesQueue; /**< Stores all the recognized 
+                                          sentences during the listen mode */
+
+        bool m_listenActivated; /**< Indicates when to start to enqueue the 
+                                  recognized sentences */
 
         std::string m_recoSentencesTopic; /**< Stores the name of the 
                                             topic where the recognized
@@ -24,13 +58,22 @@ class SpeechRecognitionStatus
         /**
          * @brief Current recognized sentence callback
          * 
-         * Updates the robot's recognized speech sentence when the 
-         * corresponding topic is updated.
+         * Updates the robot's recognized speech sentences when the 
+         * corresponding topic is updated and stores it in the recoSentences
+         * queue when the listening mode is activated.
          *
-         * @param recoSentenceMsg The new value of the topic when it's updated. 
+         * @param recoSentencesMsg The new value of the topic when it's updated. 
+         * @return void
          */
         void recoSentenceCallback(
-                const hri_msgs::RecognizedSpeech::ConstPtr& msg);
+                const hri_msgs::RecognizedSpeech::ConstPtr& recoSentencesMsg);
+
+        /**
+         * @brief Remove all the element of a recognize sentence  queue.
+         *
+         * @param queue[in,out] The queue to empty.
+         */
+        void clearRecoSentencesQueue (std::queue<RecognizedSentence> &queue);
     public:
         /**
          * @brief Class constructor
@@ -45,13 +88,24 @@ class SpeechRecognitionStatus
                 recoSentencesTopic= "recognizedSpeech");
         
         /**
-         * @brief Returns the value of the current head pan and tilt by 
-         * reference.
-         * 
-         * @param headPan Stores the value of the current head pan.
-         * @param headPan Stores the value of the current head tilt.
-         * @return void
+         * @brief Activates the listening mode i.e. start the storing of the
+         * recognized sentences.
          */
-        //void getHeadPose(float &headPan, float &headTilt);
+        void startListening();
+
+        /**
+         * @brief Deactivates the listening mode i.e. stop the storing of the
+         * recognized sentences.
+         */
+        void stopListening();
+
+        /**
+         * @brief Clears the recognized sentences queue (filled during the 
+         * listening mode).
+         *
+         * This method only clears the queue when the lisening mode is 
+         * deactivated.
+         */
+        void clearListenedSentencesQueue();
 };
 #endif
